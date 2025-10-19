91832073fc183cd5204e191ee01e4b0862810a2c
Sirehtea <turgayfatih.yasar@hotmail.com>
2025-10-19 23:46:28 +0200

    Add script for TV and Radio

---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRadio : MonoBehaviour
+{
+    public AudioSource radioAudio;
+    public float startDelay = 10f;
+    private bool isOn = false;
+
+    void Start()
+    {
+        StartCoroutine(StartRadioAfterDelay());
+    }
+
+    IEnumerator StartRadioAfterDelay()
+    {
+        yield return new WaitForSeconds(startDelay);
+        radioAudio.Play();
+        isOn = true;
+        Debug.Log("Radio started automatically");
+    }
+
+    void OnMouseDown()
+    {
+        // Wordt ook getriggerd door XR-ray interactor bij ‘click/select’,
+        // zolang je collider heeft en interactie toestaat
+        if (isOn)
+        {
+            radioAudio.Stop();
+            isOn = false;
+            Debug.Log("Radio turned off");
+        }
+        else
+        {
+            radioAudio.Play();
+            isOn = true;
+            Debug.Log("Radio turned on");
+        }
+    }
+}
---
+++ Assets/Scripts/Tv.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TVControllerXR : MonoBehaviour
+{
+    [Header("Components")]
+    public VideoPlayer videoPlayer;          // Assign your Video Player here
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable; // Assign your XR Grab Interactable (same object)
+
+    [Header("Settings")]
+    public float startDelay = 5f;
+
+    private bool isOn = false;
+
+    void Start()
+    {
+        // Make sure both references exist
+        if (videoPlayer == null)
+            videoPlayer = GetComponent<VideoPlayer>();
+
+        if (grabInteractable == null)
+            grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+
+        // Subscribe to XR grab/activate event
+        if (grabInteractable != null)
+            grabInteractable.selectEntered.AddListener(OnGrab);
+
+        // Start video after delay
+        Invoke(nameof(StartTV), startDelay);
+    }
+
+    void OnDestroy()
+    {
+        if (grabInteractable != null)
+            grabInteractable.selectEntered.RemoveListener(OnGrab);
+    }
+
+    void StartTV()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.Play();
+            isOn = true;
+        }
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        ToggleTV();
+    }
+
+    public void ToggleTV()
+    {
+        if (videoPlayer == null) return;
+
+        if (isOn)
+        {
+            videoPlayer.Pause();
+            isOn = false;
+        }
+        else
+        {
+            videoPlayer.Play();
+            isOn = true;
+        }
+    }
+}
